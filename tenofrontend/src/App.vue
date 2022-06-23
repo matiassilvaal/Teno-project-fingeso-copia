2876d246c946ccd91da0070ac2f9d42dbc1c9fdf
Andres Ijurra <andres.ijurra@usach.cl>
2022-06-23 15:37:29 -0400

    frontend init

--- tenofrontend/src/App.vue
+++ tenofrontend/src/App.vue
@@ -1,10 +1,29 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav class="navbar navbar-expand-lg" style="background-color: #004582;">
+      <div class="container-fluid">
+        <router-link to="/" class="navbar-brand text-light">TENO-DGDE</router-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <router-link style="color: #ff8414;" to="/" class="nav-link">Home</router-link>
+            <router-link style="color: #ff8414;" to="/about" class="nav-link">About</router-link>
+            <router-link style="color: #ff8414;" to="#" class="nav-link">Denuncias</router-link>
+          </div>
+        </div>
+      </div>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
